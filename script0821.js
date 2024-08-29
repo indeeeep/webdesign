ed1bb9a62997d1fb424ba9b177474e471341f615
indeeeep <indeeeep@gmail.com>
2024-08-29 15:15:32 +0900

    about me 수정하는 도중 top, left 수치들 많이 수정해야 할 것 같아서 커밋함

---
+++ script0821.js
@@ -0,0 +1,28 @@
+/*네비게이션 메뉴 눌렀을 때 이동하는 화면이 부드럽게 되는 거*/
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('.navbar a[href^="#"]');
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    });
+  });
+});
+
+function setVh() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+window.addEventListener("resize", setVh);
+setVh();
